bb47dede15efbcf1e0538edd3e749afedba69d9d
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-02-10 19:12:07 -0300

    iniciando

---
+++ src/com/unab/Huevo.java
@@ -0,0 +1,17 @@
+package com.unab;
+
+/*
+@author Luis Zenteno
+*/
+
+public class Huevo {
+
+    int fila, columna, puntajeLanzamiento;
+
+    public Huevo(int fila, int columna, int puntajeLanzamiento) {
+        this.fila = fila;
+        this.columna = columna;
+        this.puntajeLanzamiento = puntajeLanzamiento;
+    }
+
+}
---
+++ src/com/unab/Trupalla.java
@@ -0,0 +1,19 @@
+package com.unab;
+
+/**
+ * @author Luis Zenteno
+ *
+ */
+public class Trupalla extends Carro {
+
+    int nivelArmadura;
+    String nombre;
+
+    public Trupalla(String cantOcupantes, String fechaIngreso, int fila, int columna, int nivelArmadura,
+            String nombre) {
+        super(cantOcupantes, fechaIngreso, fila, columna);
+        this.nivelArmadura = nivelArmadura;
+        this.nombre = nombre;
+    }
+}
+
